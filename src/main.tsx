fb1a38e5d0427957a90fba4161aaa781d1b43bf7
LiamFrost <frostnova986@gmail.com>
2026-01-15 03:02:56 -0800

    Replace BrowserRouter with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./app/App";
 import { I18nProvider } from "./lib/i18n";
@@ -9,9 +9,9 @@ import "./styles/globals.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <I18nProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </I18nProvider>
   </React.StrictMode>
 );
